08ccb31abb27b9d1664ce96fc440cd39b2cc7b6f
Maxime Song <me@cppdo.com>
2014-05-15 23:51:00 +0800

    start to write some scripts to crawl the top sites

--- tools/index.js
+++ tools/index.js
@@ -1,17 +1,33 @@
 var jsdom = require('jsdom');
 var fs = require('fs');
+var http = require('http');
 
 var jquery = fs.readFileSync('./node_modules/jquery/dist/jquery.js');
 
-jsdom.env({
-  html: '<div>Hello</div><a href="www.sina.com.cn">Click it</a>',
-  src: [jquery],
-  done: function(error, window) {
-    var $ = window.$;
-    $('a').each(function() {
-      console.log($(this).text());
-    });
-  }
-});
-  
+var url_prefix = 'http://www.alexa.com/topsites/global;';
+var extract_sites = function (html) {
+  jsdom.env({
+    html: html,
+    src: [jquery],
+    done: function(error, window) {
+      var $ = window.$;
+      $('li.site-listing').each(function(index, element) {
+        console.log(element);
+      });
+    }
+  });
+};
 
+// http.get('http://www.alexa.com/topsites', function(res) {
+//   var data = '';
+//   res.on('data', function(chunk) {
+//     data += chunk;
+//   });
+//   res.on('end', function() {
+//     fs.writeFileSync('topsites', data);
+//   });
+// });
+
+var test_html = fs.readFileSync('topsites');
+
+extract_sites(test_html);
