9e73aa14498999d1140e75010fde8ea77d8110a7
Maxime Song <me@cppdo.com>
2014-05-18 23:08:45 +0800

    update the script

--- tools/index.js
+++ tools/index.js
@@ -4,30 +4,95 @@ var http = require('http');
 
 var jquery = fs.readFileSync('./node_modules/jquery/dist/jquery.js');
 
-var url_prefix = 'http://www.alexa.com/topsites/global;';
-var extract_sites = function (html) {
+var extract_sites = function (html, callback) {
   jsdom.env({
     html: html,
     src: [jquery],
     done: function(error, window) {
       var $ = window.$;
+      var sites_info = {};
       $('li.site-listing').each(function(index, element) {
-        console.log(element);
+        var name = $('.desc-paragraph a', element).text();
+        var info_url = $('.desc-paragraph a', element).attr('href');
+        sites_info[name] = {
+          info_url: info_url
+        };
       });
+      callback(sites_info);
     }
   });
 };
 
-// http.get('http://www.alexa.com/topsites', function(res) {
-//   var data = '';
-//   res.on('data', function(chunk) {
-//     data += chunk;
-//   });
-//   res.on('end', function() {
-//     fs.writeFileSync('topsites', data);
-//   });
-// });
+var download_and_save = function(url, filename, callback) {
+  console.log("get: " + url);
+  http.get(url, function(res) {
+    var data = '';
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+    res.on('end', function() {
+      console.log('end');
+      fs.writeFileSync(filename, data);
+      if (callback) {
+        callback();
+      }
+    });
+  });
+};
 
-var test_html = fs.readFileSync('topsites');
+var download_and_save_all = function(list, callback) {
+  if (list.length === 0 && callback) {
+    callback();
+  } else {
+    var pair = list.pop();
+    download_and_save(pair.url, pair.filename, function() {
+      download_and_save_all(list, callback);
+    });
+  }
+};
 
-extract_sites(test_html);
+var mkdir = function(path) {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+};
+
+var download_data = function() {
+  var alexa_url = 'http://www.alexa.com';
+  var topsites_prefix = alexa_url + '/topsites/global;';
+  var download_path = 'download';
+  var sites_info_path = download_path + '/siteinfo';
+  var sites_list_path = download_path + '/topsites';
+
+  var alexa2_url = 'http://alexa.chinaz.com/?domain=';
+  mkdir(download_path);
+  mkdir(sites_info_path);
+  mkdir(sites_list_path);
+
+  (function helper(i) {
+    if (i >= 20) {
+      return;
+    }
+    var html = fs.readFileSync(sites_list_path + '/sites' + i);
+    extract_sites(html, function(sites) {
+      var pair_list = [];
+      for (var site in sites) {
+        if (!fs.existsSync(sites_info_path + '/' + site)) {
+          pair_list.push({
+            url: alexa2_url + site,
+            filename: sites_info_path + '/' + site
+          });
+        }
+      }
+      download_and_save_all(pair_list, function(){
+        helper(i + 1);
+      });
+    });    
+  })(0);
+    
+};
+
+download_data();
+//var test_html = fs.readFileSync('topsites');
+
+//extract_sites(test_html);
